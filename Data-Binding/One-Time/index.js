8aff169579ba6667d5af71d87dce216cf462f88a
Nishant Gautam <ngautam0@live.com>
2017-07-30 17:36:56 +0530

    Data Binding one way two way and one time

---
+++ Data-Binding/One-Time/index.js
@@ -0,0 +1,12 @@
+var app = angular.module("sample",[])
+
+app.controller("emp",["$scope", function ($scope) {
+  $scope.a = 10
+  $scope.b = 20
+
+  $scope.doDoubleAndSum = function(){
+    $scope.a = $scope.a * 2
+    $scope.b = $scope.b * 2
+    $scope.sum = $scope.a + $scope.b
+  }
+}]);
